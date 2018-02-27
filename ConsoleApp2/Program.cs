e3a2dc0c310e5c8256140584615ffcdf3f711fc6
hi-tunisia <36881385+hi-tunisia@users.noreply.github.com>
2018-02-27 10:14:40 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ namespace ConsoleApp2
             modle.Name = "hello Tunisia";
             Console.WriteLine(modle.Name);
             Console.ReadLine();
+            //note
         }
     }
 }
